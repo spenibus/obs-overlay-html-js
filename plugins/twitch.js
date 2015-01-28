20561282a63004974a19db4a26b8f30a4ca16d37
spenibus <git@spenibus.net>
2015-01-28 21:01:34 +0100

    - Twitch plugin: vars are now initiated at startup and timeout is handled

--- plugins/twitch.js
+++ plugins/twitch.js
@@ -37,12 +37,17 @@ available variables:
       // load twitch streams data
       obsOverlay.fetchUrl(
          'https://api.twitch.tv/kraken/streams/'+channel+'?t='+timestamp,
+         // onload
          function(r) {
             r = JSON.parse(r.target.responseText);
             r = r && r.stream && r.stream.viewers != null ? r.stream.viewers : 'n/a'
 
             // export var
             obsOverlay.setVar('twitch.viewers.count', r);
+         },
+         // timeout
+         function(r) {
+            obsOverlay.setVar('twitch.viewers.count', 'n/a');
          }
       );
 
@@ -50,15 +55,26 @@ available variables:
       // load twitch chatters data
       obsOverlay.fetchUrl(
          'http://tmi.twitch.tv/group/user/'+channel+'/chatters?t='+timestamp,
+         // onload
          function(r) {
             r = JSON.parse(r.target.responseText);
             r = r && r.chatter_count != null ? r.chatter_count : 'n/a'
 
             // export var
             obsOverlay.setVar('twitch.chatters.count', r);
+         },
+         // timeout
+         function(r) {
+            obsOverlay.setVar('twitch.chatters.count', 'n/a');
          }
       );
    }
+
+
+   // init vars
+   obsOverlay.setVar('twitch.viewers.count',  'n/a');
+   obsOverlay.setVar('twitch.chatters.count', 'n/a');
+
 
    // at start
    update();
